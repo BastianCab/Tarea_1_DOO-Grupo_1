f3247bc901baab75a8bbd27b71eff77c1c28a0a3
Bastian <bacabezas2025@udec.cl>
2026-04-25 19:38:44 -0400

    Modificación enum Constantes

--- Expendedora/src/main/java/Constantes.java
+++ Expendedora/src/main/java/Constantes.java
@@ -1,17 +1,23 @@
 public enum Constantes {
-    COCACOLA(200),
-    SPRITE(150),
-    FANTA(150),
-    SUPER8(100),
-    SNICKERS(200);
+    COCACOLA(200, 1),
+    SPRITE(100, 2),
+    FANTA(100, 3),
+    SUPER8(100, 4),
+    SNICKERS(200, 5);
 
     private final int Precio;
+    private final int numProducto;
 
-    Constantes(int precio) {
+    Constantes(int precio, int numProducto) {
         this.Precio = precio;
+        this.numProducto = numProducto;
     }
 
     public int getPrecio() {
         return Precio;
+    }
+
+    public int getNumProducto() {
+        return numProducto;
     }
 }
