f86517701978b046b027b9e374e603b81592bea2
Bastian <bacabezas2025@udec.cl>
2026-04-24 23:12:46 -0400

    Agregado a clase Moneda: Interfaz Comparable y Override de compareTo()

--- Expendedora/src/main/java/Moneda.java
+++ Expendedora/src/main/java/Moneda.java
@@ -1,4 +1,4 @@
-public abstract class Moneda {
+public abstract class Moneda implements Comparable<Moneda> {
     public Moneda() {}
 
     public Moneda getSerie() {
@@ -6,4 +6,14 @@ public abstract class Moneda {
     }
 
     public abstract int getValor();
+
+    @Override
+    public int compareTo(Moneda mon) {
+        if (this.getValor() < mon.getValor())
+            return -1;
+        else if (this.getValor() > mon.getValor())
+            return 1;
+        else
+            return 0;
+    }
 }
