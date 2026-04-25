53120a9308562903777756f885b72984c47457f2
Bastian <bacabezas2025@udec.cl>
2026-04-24 22:37:52 -0400

    Añadido clase: Moneda. Y subclases: Moneda100, Moneda500 y Moneda1000

---
+++ Expendedora/src/main/java/Moneda100.java
@@ -0,0 +1,8 @@
+public class Moneda100 extends Moneda {
+    public Moneda100() {}
+
+    @Override
+    public int getValor() {
+        return 100;
+    }
+}
---
+++ Expendedora/src/main/java/Moneda500.java
@@ -0,0 +1,8 @@
+public class Moneda500 extends Moneda {
+    public Moneda500() {}
+
+    @Override
+    public int getValor() {
+        return 500;
+    }
+}
